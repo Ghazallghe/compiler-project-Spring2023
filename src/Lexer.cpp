691ffb3334ad966f901d8935a9fa738102e81019
Ghazaal <ghazaal.laghaee@gmail.com>
2023-05-22 00:25:12 +0330

    "fix: remove commented code"

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,103 +1,9 @@
-// #include "Lexer.h"
-
-// // classifying characters
-// namespace charinfo
-// {
-//     LLVM_READNONE inline bool isWhitespace(char c)
-//     {
-//         return c == ' ' || c == '\t' || c == '\f' ||
-//                c == '\v' || c == '\r' || c == '\n';
-//     }
-
-//     LLVM_READNONE inline bool isDigit(char c)
-//     {
-//         return c >= '0' && c <= '9';
-//     }
-
-//     LLVM_READNONE inline bool isLetter(char c)
-//     {
-//         return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-//     }
-// }
-
-// // ignore whitespaces
-// void Lexer::next(Token &token)
-// {
-//     while (*BufferPtr && charinfo::isWhitespace(*BufferPtr))
-//     {
-//         ++BufferPtr;
-//     }
-
-//     // make sure we didn't reach the end of input
-//     if (!*BufferPtr)
-//     {
-//         token.Kind = Token::eoi;
-//         return;
-//     }
-
-//     // collect characters and check for keywords or ident
-//     if (charinfo::isLetter(*BufferPtr))
-//     {
-//         const char *end = BufferPtr + 1;
-//         while (charinfo::isLetter(*end))
-//             ++end;
-//         llvm::StringRef Name(BufferPtr, end - BufferPtr);
-//         Token::TokenKind kind;
-//         if (Name == "type")
-//             kind = Token::KW_type;
-//         else if (Name == "int")
-//             kind = Token::KW_int;
-//         else
-//             kind = Token::ident;
-//         // populate the token
-//         formToken(token, end, kind);
-//         return;
-//     }
-//     // check for numbers
-//     else if (charinfo::isDigit(*BufferPtr))
-//     {
-//         const char *end = BufferPtr + 1;
-//         while (charinfo::isDigit(*end))
-//             ++end;
-//         formToken(token, end, Token::number);
-//         return;
-//     }
-//     else
-//     {
-//         switch (*BufferPtr)
-//         {
-// #define CASE(ch, tok) \
-//     case ch:          \
-//         formToken(token, BufferPtr + 1, tok);
-//             break;
-//             CASE('+', Token::plus);
-//             CASE('-', Token::minus);
-//             CASE('*', Token::star);
-//             CASE('/', Token::slash);
-//             CASE('(', Token::l_paren);
-//             CASE(')', Token::r_paren);
-//             CASE(';', Token::semicolon);
-//             CASE(',', Token::comma);
-//             CASE('=', Token::equal);
-// #undef CASE
-//         default: // checking for unexpected characters
-//             formToken(token, BufferPtr + 1, Token::unknown);
-//         }
-//         return;
-//     }
-// }
-
-// void Lexer::formToken(Token &Tok, const char *TokEnd, Token::TokenKind Kind)
-// {
-//     Tok.Kind = Kind;
-//     Tok.Text = llvm::StringRef(BufferPtr, TokEnd - BufferPtr);
-//     BufferPtr = TokEnd;
-// }
-
 #include "Lexer.h"
 
+// classifying characters
 namespace charinfo
 {
+    // ignore whitespaces
     LLVM_READNONE inline bool isWhitespace(char c)
     {
         return c == ' ' || c == '\t' || c == '\f' || c == '\v' ||
@@ -121,19 +27,19 @@ void Lexer::next(Token &token)
     {
         ++BufferPtr;
     }
+    // make sure we didn't reach the end of input
     if (!*BufferPtr)
     {
         token.Kind = Token::eoi;
         return;
     }
+    // collect characters and check for keywords or ident
     if (charinfo::isLetter(*BufferPtr))
     {
         const char *end = BufferPtr + 1;
         while (charinfo::isLetter(*end))
             ++end;
         llvm::StringRef Name(BufferPtr, end - BufferPtr);
-        //        Token::TokenKind kind =
-        //            Name == "with" ? Token::KW_type : Token::ident;
         Token::TokenKind kind;
         if (Name == "type")
             kind = Token::KW_type;
@@ -141,9 +47,11 @@ void Lexer::next(Token &token)
             kind = Token::KW_int;
         else
             kind = Token::ident;
+        // generate the token
         formToken(token, end, kind);
         return;
     }
+    // check for numbers
     else if (charinfo::isDigit(*BufferPtr))
     {
         const char *end = BufferPtr + 1;
